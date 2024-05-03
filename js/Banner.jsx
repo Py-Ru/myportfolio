03704aee1b728fc24890e3ba1dd60c4cf6c816cc
Py-Ru <ashvinmaruti@gmail.com>
2024-05-03 10:19:35 +0100

    banner for mobile done

--- js/Banner.jsx
+++ js/Banner.jsx
@@ -1,7 +1,7 @@
 function Banner() {
     React.useEffect(() => {
         let typed = new Typed('.animated-text', {
-            strings: ['Junior Web Developer', 'Scribe', 'and Cool Guy'],
+            strings: ['Junior Web Developer', 'Scribe', 'and a Cool Guy'],
             typeSpeed: 60,
             backSpeed: 60,
             loop: true,
@@ -14,9 +14,24 @@ function Banner() {
         };
     }, []);
     return (
-        <section id="banner">
-            <div>I am</div>
-            <div className="animated-text inline"></div>
+        <section id="banner" className="flex flex-col justify-center items-center w-[90%] mx-auto">
+            <div id="full-name" className="w-full my-[24px] font-bold text-[3rem] text-center leading-[1.2] break-words">I am Amyr Yousufzai <br /> 
+                <span className="animated-text inline text-[24px] text-[#ba68c8] font-[400px] leading-[1.2]"></span>
+            </div>
+
+            <p className="text-center">To put into practice what I have accumulated during my learning process while expanding my experience, knowledge and expertise in web development.</p>
+            
+            <div class="w-full mt-10">
+                <a href=".\assets\amyr-cv.docx" download class="block text-center bg-white-300 hover:bg-blue-600 border border-violet-950 text-black font-semibold mb-[10px] py-2 px-4 rounded-full shadow">
+                    Download CV
+                </a>
+                <button onClick="getRandomQuote()" class="block w-full mb-[60px] text-white text-center bg-[#ba68c8] hover:bg-blue-600 text-black font-semibold py-2 px-4 rounded-full shadow">
+                    Get a quote
+                </button>
+
+                <img src="assets/images/coding-bro.svg" alt="" />
+                
+            </div>
         </section>
     );
 }
